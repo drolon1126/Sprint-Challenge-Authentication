5cbdf01f295715ff50351fff9203b06e711472ea
danRolon <daniel.rolon@yahoo.com>
2019-10-18 18:18:25 -0500

    switch async await to .then

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -5,20 +5,22 @@ const bcrypt = require('bcryptjs');
 const jwt =require('jsonwebtoken');
 const secrets = require('../config/secrets.js');
 
+beforeEach(async () => {
+  await db('users').truncate();
+});
+
 describe('auth-router.js',()=>{
-  beforeEach(async () => {
-    await db('users').truncate();
-  });
 
   describe('POST /register',()=>{
     it('should return json with a 201 http status code', ()=>{
       return request(server).post('/api/auth/register')
-        .send( {username:"john",password:"123"} )
+        .send({username:"steve",password:"123"})
         .then(res=>{
           expect(res.type).toMatch(/json/i);
           expect(res.status).toEqual(201);
       });
     });
+
     it('it should return an obj with username and hashed password', async ()=>{
       const res = await request(server).post('/api/auth/register')
       .send({username:"drolon",password:"123"});
@@ -29,16 +31,17 @@ describe('auth-router.js',()=>{
     });
   });
   describe('POST /login',()=>{
-    it('should return json with a 200 http status code', async ()=>{
-      await request(server).post('/api/auth/register')
-        .send( {username:"john",password:"123"} );
-      
-      const res = await request(server).post('/api/auth/login')
-        .send( {username:"john",password:"123"} );
-
-      expect(res.type).toMatch(/json/i);
-      expect(res.status).toEqual(200);
-
+    it('should return json with a 200 http status code', ()=>{
+      return request(server).post('/api/auth/register')
+      .send({username:"drolon",password:"123"})
+      .then(r=>{
+        return request(server).post('/api/auth/login')
+        .send({username:"drolon",password:"123"})
+        .then(res=>{
+          expect(res.type).toMatch(/json/i);
+          expect(res.status).toEqual(200);
+          })
+      })
     });
     it('should return an object with a message and token', async ()=>{
       await request(server).post('/api/auth/register')
@@ -62,5 +65,5 @@ describe('auth-router.js',()=>{
         })
 
     });
-  });
+   });
 })
--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -1,9 +1,8 @@
 const request = require('supertest'); 
 const server = require('../api/server.js');
 const db = require('../database/dbConfig.js');
-const bcrypt = require('bcryptjs');
-const jwt =require('jsonwebtoken');
-const secrets = require('../config/secrets.js');
+
+
 
 describe('jokes-router.js',()=>{
   beforeEach(async () => {
@@ -14,16 +13,20 @@ describe('jokes-router.js',()=>{
       const res = await request(server).get("/api/jokes");
       expect(res.status).toEqual(400);
     });
-    it('should return status 200 with a token', async ()=>{
-      await request(server).post('/api/auth/register')
-      .send( {username:"john",password:"123"} );
-    
-      const login = await request(server).post('/api/auth/login')
-      .send( {username:"john",password:"123"} );
-      
-      const res = await request(server).get("/api/jokes").set('authorization', login.body.token);
 
-      expect(res.status).toEqual(200);
+    it('should return status 200 with a token', ()=>{
+    return request(server).post('/api/auth/register')
+      .send( {username:"john",password:"123"} )
+      .then(r=>{
+        return request(server).post('/api/auth/login')
+          .send( {username:"john",password:"123"} )
+          .then(login=>{
+            request(server).get("/api/jokes").set('authorization', login.body.token)
+            .then(res=>{
+              expect(res.status).toEqual(200);
+            })
+          })
+      })      
     });
   })
 })
